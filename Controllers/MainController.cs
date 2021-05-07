2a84c90df424f017fa07627658fe0a54593b1f52
Ramon Arruda <ramon.arruda@capgemini.com>
2021-05-07 13:24:06 -0300

    first commit

---
+++ Controllers/MainController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace serviceB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("service B Ok...");
+        }
+
+        [HttpGet("test")]
+        public IActionResult GetTest(string someString)
+        {
+            _logger.LogInformation("Get test called!");
+            return Ok($"You passed {someString} as parameter.");
+        }
+    }
+}
